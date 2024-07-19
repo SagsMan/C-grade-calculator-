b855db040040baf89dae8cbb5af155647dca63d2
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2024-07-19 18:09:15 +0100

    Add files via upload
    C++ grade calculator develop by SagsMan

---
+++ Grade calculator c++.cxx
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int score;
+	//Grade calculator
+	cout<<"Enter Score"<<endl;
+	cin>>score;
+	if (score>=70)
+	cout<<"A"<<endl;
+	else if (score>=60)
+	cout<<"B"<<endl;
+		else if (score>=50)
+	cout<<"C"<<endl;
+		else if (score>=45)
+	cout<<"D"<<endl;
+		else if (score>=40)
+	cout<<"E"<<endl;
+		else if (score>=39)
+	cout<<"FAIL"<<endl;
+	
+	
+return 0;}
